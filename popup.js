8e30e61aee13e890959e4493905dd10dc63c67ec
cjfcode <inbox.chrisf@gmail.com>
2021-05-18 02:44:17 -0400

    colorized popup text

--- popup.js
+++ popup.js
@@ -1,24 +1,24 @@
-// Initialize button with user's preferred color.
-let changeColor = document.getElementById("changeColor");
+// events
+document.addEventListener('DOMContentLoaded', setMulticolorText);
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// functions
+function setMulticolorText()
+{
+  // gets all objects using the title class in popup.css
+  let title = document.getElementsByClassName("title").item(0);
 
-// When the button is clicked, inject setPageBackgroundColor into current page.
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  // the colors to cycle between for the letters
+  let colors = new Array("rgb(207, 0, 0)", "rgb(0, 207, 26)", "rgb(14, 97, 234)"); // red, green, blue
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
+  // the HTML code to replace the title object's HTML with
+  let content = "";
 
-// The body of this function will be executed as a content script inside the
-// current page.
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+  // loop over the title's text and append code to the content string that changes the color of the individual letters
+  for (var i = 0; i < title.textContent.length; i++)
+  {
+    content += "<span style=\"color:" + colors[(i % colors.length)] + ";\">" + title.textContent[i] + "</span>"
+  }
+
+  // replace the title's HTML
+  title.innerHTML = content;
 }
