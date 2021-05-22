381a04d262baf89e28d5b37cd17686e2dada7d18
Richard Rivas <rrivas888@yahoo.com>
2021-05-22 17:04:18 -0500

    Added popup.js button clicking function.

--- popup.js
+++ popup.js
@@ -1,23 +1,28 @@
-// events
+// Events
 document.addEventListener('DOMContentLoaded', setMulticolorText);
 
-// functions
+// Functions
 function setMulticolorText()
 {
-  // gets the title object
+  // Receives the title object.
   let title = document.getElementsByClassName("title").item(0);
 
-  // the colors to cycle between for the letters
+  // The colors indicated to cycle between the letters.
   let colors = new Array("rgb(207, 0, 0)", "rgb(0, 207, 26)", "rgb(14, 97, 234)");
 
-  // the HTML code to replace the title object's HTML with
+  // The HTML code to replace the title object's HTML with.
   let content = "";
 
-  // loop over the title's text and append code to the content string that changes the color of the individual letters
+  // Loop over the title's text and append code to the content string that changes the color of the individual letters.
   for (var i = 0; i < title.textContent.length; i++)
   {
     content += "<span style=\"color:" + colors[(i % colors.length)] + ";\">" + title.textContent[i] + "</span>"
   }
 
   title.innerHTML = content;
+}
+
+// Response when button is clicked.
+document.getElementById('everySearch').onclick = function() {
+  window.alert("Hello World.")
 }
