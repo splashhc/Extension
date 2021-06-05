0b581dbe8922824fb702dd58ef4b9f9be78bb7b0
Richard Rivas <rrivas888@yahoo.com>
2021-06-04 21:11:50 -0500

    Added wikipediascraper.js execution

--- background.js
+++ background.js
@@ -2,11 +2,23 @@ chrome.tabs.onUpdated.addListener(detect_search);
 
 function detect_search(_tabId, pageinfo, tab)
 {
-    // we only want to run the script once per page load, so check first to see if the page has finished loading
+    // We only want to run the script once per page load, so check first to see if the page has finished loading.
     if(pageinfo.status === 'complete')
     {
         chrome.scripting.executeScript({ 
             target: {tabId: _tabId},
             files: ["search_detector.js"]});
+    }
+}
+
+chrome.tabs.onUpdated.addListener(detect_search);
+
+function detect_search(_tabId, pageinfo, tab)
+{
+    if(pageinfo.status === 'complete')
+    {
+        chrome.scripting.executeScript({ 
+            target: {tabId: _tabId},
+            files: ["wikipediascraper.js"]});
     }
 }
