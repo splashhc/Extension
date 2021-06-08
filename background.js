bc91d805a3d5778a8b976517cfd793dc55562833
cjfcode <inbox.chrisf@gmail.com>
2021-06-08 13:17:16 -0400

    added toast notifications and summary scraping

--- background.js
+++ background.js
@@ -1,16 +1,38 @@
-chrome.tabs.onUpdated.addListener(detectSearch);
+chrome.webNavigation.onHistoryStateUpdated.addListener(detectSearch); // for now, this only partially works with Bing. Full compatibility with Google
 
-function detectSearch(_tabId, info)
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse)
+{   
+    // console.log("The message is: " + request.message);
+    
+        var options =
+        {
+            type : "basic",
+            iconUrl: "https://www.google.com/favicon.ico",
+            title : "Did you know...",
+            message: request.message,
+            priority: 1,
+            buttons:[ { title:'dismiss'}, {title:'learn more'} ],
+            isClickable: true
+        }
+        chrome.notifications.create("", options, function() { console.log(chrome.runtime.lastError); });    
+
+    sendResponse({message: 'success'});
+    return true;
+});
+
+
+function detectSearch(details)
 {
-    // we only want to run the script once per page load, so check first to see if the page has finished loading
-    if(info.status === 'complete')
+    // we only want to run the script once per page load and for only one frame, so check first to see if the page is a new tab and we're only
+    // triggering on the first frame
+    if(!details.url.match('chrome://new-tab-page/') && details.frameId == 0)
     {
         chrome.scripting.executeScript({ 
-            target: {tabId: _tabId},
-            files: ["search_detector.js"]});
-            
+            target: {tabId: details.tabId},
+            files: ["search_detector.js"]}, _=> chrome.runtime.lastError);
+        
         chrome.scripting.executeScript({ 
-            target: {tabId: _tabId},
-            files: ["wikipediascraper.js"]});
+            target: {tabId: details.tabId},
+            files: ["wikipediascraper.js"]}, _=> chrome.runtime.lastError);
     }
 }
--- wikipediascraper.js
+++ wikipediascraper.js
@@ -1,32 +1,58 @@
 chrome.storage.local.get("query", function(data)
 {
-  let url = data.query;
-  console.log("url is: " + url); 
+  let query = data.query; // change this if we're using randomized facts rather than context facts
+
+  console.log("The query is: " + query);
+  processFacts(query);
+
+  //console.log("url is: " + url); 
 });
 
-async function getWikipediaUrlFromQuery(query)
+async function processFacts(searchedQuery)
 {
-    let url = encodeURI(`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${query}`);
-    let response = await fetch(url);
-    let data = await response.json();
-    let pageId = data.query.search[0].pageid;
-    let wikipediaUrl = `https://en.wikipedia.org/?curid=${pageId}`;
-    //console.log(wikipediaUrl);
-    return wikipediaUrl;
+  let query = searchedQuery; // possibly change this for randomized facts
+  let urlAndPageId = await getUrlAndPageIdFromQuery(query);
+  let summary = await getSummaryFromPageId(urlAndPageId[0]);
+  let facts = splitSummaryIntoFacts(summary);
+  let result = getRandomFactFromFactArray(query, facts);
+
+  chrome.runtime.sendMessage({message: result}); //
+
+  
+  //console.log(result); // instead of logging the result, we will need to display the result somehow
 }
 
-// not yet working since we don't yet have a URL
-// function getWikipediaData(url)
-// {
-//   fetch(url)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(response){
-//       html_code = response["parse"]["text"]["*"];
-//       parser = new DOMParser();
-//       html = parser.parseFromString(html_code, "text/html");
-//       var tables = html.querySelectorAll(".wikitable");
-//       console.log(tables);
-//     })
-// }
+// element 0 is pageId, element 1 is wikipedia URL
+async function getUrlAndPageIdFromQuery(query)
+{
+  let url = encodeURI(`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${query}`);
+  let response = await fetch(url);
+  let data = await response.json();
+  let pageId = await data.query.search[0].pageid;
+  let wikipediaUrl = `https://en.wikipedia.org/?curid=${pageId}`;
+  return [pageId, wikipediaUrl];
+}
+
+async function getSummaryFromPageId(pageId)
+{
+  let url = `https://en.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${pageId}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  let summary = await data.query.pages[pageId].extract;
+  return summary;
+}
+
+// returns a string array of the summary split into individual sentences
+function splitSummaryIntoFacts(summary)
+{
+  // str.replace(/\.(?!\d)|([^\d])\.(?=\d)/g,'$1.|') this regex may be better
+  return summary.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
+}
+
+function getRandomFactFromFactArray(query, facts)
+{
+  let maxIndex = facts.length;
+  let randomIndex = Math.floor((Math.random() * maxIndex));
+  let formattedFacts = `${query}: ${facts[randomIndex]}`;
+  return formattedFacts;
+}
